7d095f6617c75cf8f65ac23fa2e7130e5d348565
Chihiro Tomioka <chi.tomioka@gmail.com>
2023-03-26 00:57:14 +0900

    モデルとマイグレーションの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: "佐藤", age: 17)
+User.create(name: "鈴木", age: 21)
